1bf47d7f377e0f739edff8d4d37fcd60b5bd601c
arkadeep22 <101667807+arkadeep22@users.noreply.github.com>
2023-10-30 22:56:44 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 // export const server = "http://localhost:8000/api/v2";
 
-export const server = "http://arkaopssss.onrender.com/api/v2";
+export const server = "https://arkaopssss.onrender.com/api/v2";
